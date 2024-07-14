5df2047f054330d2cda470f9435eb130897e9dba
Krish085p <97827184+Krish085p@users.noreply.github.com>
2024-07-14 16:15:07 +0530

    Changes

--- Client/src/Components/Navbar/Navbar.jsx
+++ Client/src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import { ShopContext } from "../../Context/ShopContext";
 
 export const Navbar = () => {
   const [menu, setMenu] = useState("shop");
-  const { getTotalCartItems } = useContext(ShopContext);
+  const { getTotalCartItems, handleLogout } = useContext(ShopContext);
   const totalCartItems = getTotalCartItems();
   const menuRef = useRef();
 
@@ -20,7 +20,7 @@ export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="nav-logo">
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo"/>
         <p>SHOPPER</p>
       </div>
       <img
@@ -57,7 +57,7 @@ export const Navbar = () => {
       </ul>
       <div className="nav-login-cart">
       {localStorage.getItem("auth-token")? 
-      <button onClick={() => {localStorage.removeItem("auth-token"); window.location.replace("/");}}>Logout</button>:
+      <button onClick={handleLogout}>Logout</button>:
        <Link to="/login"><button>Login</button></Link>}
 
         <Link to="/cart">
