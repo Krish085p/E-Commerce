49037bb915e1ece121a1f6fee582cb79f49d3a8c
Krish085p <97827184+Krish085p@users.noreply.github.com>
2024-08-12 23:22:36 +0530

    admin fixed

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Admin from './Pages/Admin/Admin'; // Import the Admin component (assuming this path is correct)
-import 'dotenv'
 
 const App = () => {
   return (
