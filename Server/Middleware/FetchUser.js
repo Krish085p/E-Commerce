d20d215e4504b361def24e2b756da6062bcceed8
Krish085p <97827184+Krish085p@users.noreply.github.com>
2024-08-21 23:09:24 +0530

    fetchuser middleware

---
+++ Server/Middleware/FetchUser.js
@@ -0,0 +1,19 @@
+const fetchUser = async (req, res, next) => {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    
+    console.log("Hell0", token)
+    
+    if (!token)
+      return res
+        .status(401)
+        .json({ errors: "Please authenticate using a valid token" });
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded.user;
+      next();
+    } catch (error) {
+      res.status(401).json({ errors: "Invalid token" });
+    }
+};
+
+module.exports = fetchUser;
